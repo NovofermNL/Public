6a219220631e47d6daabdc7135eb7ebec11e0fc6
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-02-20 21:32:05 +0100

    Create Deploy_Win11_24H2.ps1

---
+++ Deploy_Win11_24H2.ps1
@@ -0,0 +1,15 @@
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI ..."
+start-sleep -Seconds 3
+
+Write-Host -ForegroundColor Green "Updating OSD Powershell Module"
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Importing OSD Powershell Module"
+Import-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Start OSD Cloud"
+Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 11 24H2 x64' -OSLicense Retail -OSEdition Pro -Zti
+
+Write-Host -ForegroundColor Green "Restarting ..."
+start-sleep -Seconds 5
+wpeutil reboot
