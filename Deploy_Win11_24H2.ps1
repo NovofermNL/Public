53ae5d17fa44bdbf59cee42baff31d7979db68e4
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-02-28 13:08:21 +0100

    Update Deploy_Win11_24H2.ps1

--- Deploy_Win11_24H2.ps1
+++ Deploy_Win11_24H2.ps1
@@ -1,1 +1,5 @@
+
+
+Edit-OSDCloudWinPE -StartOSDCloud "-OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSLanguage nl-nl -OSActivation Retail -ZTI -Restart"
+
 Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 11 24H2 x64' -OSLicense Retail -OSEdition Pro -Zti
