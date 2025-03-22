4c3f4f74bff1c467adfc60d05d108e843e730537
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-22 23:59:34 +0100

    Update and rename Deploy_Win11_24H2.ps1 to OSDCLOUD_StartURL.ps1

--- Deploy_Win11_24H2.ps1
+++
@@ -1,5 +0,0 @@
-
-
-Edit-OSDCloudWinPE -StartOSDCloud "-OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSLanguage nl-nl -OSActivation Retail -ZTI -Restart"
-
-Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 11 24H2 x64' -OSLicense Retail -OSEdition Pro -Zti
---
+++ OSDCLOUD_StartURL.ps1
@@ -0,0 +1,1 @@
+Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 11 24H2 x64' -OSLicense Volume -OSEdition Enterprise -Zti
