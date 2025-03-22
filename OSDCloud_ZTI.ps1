f2a25d3c63992521a7ef0e44bf902759f9ba0167
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-22 22:09:34 +0100

    Create OSDCloud_ZTI.ps1

---
+++ OSDCloud_ZTI.ps1
@@ -0,0 +1,47 @@
+#================================================
+#   [PreOS] Update Module
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$OSVersion = 'Windows 11'
+$OSReleaseID = '22H2'
+$OSName = 'Windows 11 22H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'en-us'
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$false
+    WindowsUpdateDrivers = [bool]$false
+    WindowsDefenderUpdate = [bool]$false
+    SetTimeZone = [bool]$False
+    ClearDiskConfirm = [bool]$False
+    }
+
+#write variables to console
+$Global:MyOSDCloud
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
