a634a775bab20712a1b3443bfd1f7bae2b2ebd43
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-23 01:21:42 +0100

    Update RemoveUnwantedApps.ps1

--- RemoveUnwantedApps.ps1
+++ RemoveUnwantedApps.ps1
@@ -3,22 +3,28 @@
 # Define a list of apps to remove
 $appxPackages = @(
     "Clipchamp.Clipchamp",
-    "Microsoft.549981C3F5F10",
+    "Microsoft.549981C3F5F10", # Cortana
     "Microsoft.BingNews",
     "Microsoft.BingWeather",
-    "Microsoft.GamingApp",
+    "Microsoft.GamingApp", # Xbox app
     "Microsoft.GetHelp",
     "Microsoft.Getstarted",
+    "Microsoft.Microsoft3DViewer",
     "Microsoft.MicrosoftOfficeHub",
     "Microsoft.MicrosoftSolitaireCollection",
     "Microsoft.MicrosoftStickyNotes",
+    "Microsoft.MixedReality.Portal",
+    "Microsoft.OneConnect", # Your Phone
     "Microsoft.People",
-    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.Print3D",
+    "Microsoft.SkypeApp",
     "Microsoft.Todos",
     "Microsoft.WindowsAlarms",
-    "microsoft.windowscommunicationsapps",
+    "Microsoft.WindowsCamera",
+    "microsoft.windowscommunicationsapps", # Mail and Calendar
     "Microsoft.WindowsFeedbackHub",
     "Microsoft.WindowsMaps",
+    "Microsoft.WindowsSoundRecorder",
     "Microsoft.WindowsStore",
     "Microsoft.WindowsTerminal",
     "Microsoft.Xbox.TCUI",
@@ -30,9 +36,26 @@ $appxPackages = @(
     "Microsoft.ZuneMusic",
     "Microsoft.ZuneVideo",
     "MicrosoftCorporationII.QuickAssist",
-    "MicrosoftWindows.Client.WebExperience"
+    "MicrosoftWindows.Client.WebExperience",
+    "MicrosoftTeams", # Teams
+    "SpotifyAB.SpotifyMusic",
+    "AdobeSystemsIncorporated.AdobePhotoshopExpress",
+    "king.com.CandyCrushSaga",
+    "king.com.CandyCrushSodaSaga",
+    "king.com.CandyCrushFriends",
+    "Playtika.CaesarsSlotsFreeCasino",
+    "Playtika.SlotomaniaFreeSlots",
+    "Duolingo-LearnLanguagesforFree",
+    "PandoraMediaInc",
+    "Wunderlist",
+    "Flipboard.Flipboard",
+    "TwitterInc.Twitter",
+    "Facebook.Facebook",
+    "Facebook.Instagram",
+    "Facebook.Messenger",
+    "NetflixInc.Netflix",
+    "TikTok.TikTok"
 )
-
 
 # Remove for current user
 foreach ($appxPackage in $appxPackages) {
