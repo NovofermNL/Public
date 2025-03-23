3c96d2da4f8a04481c3c9dfae99337e8bb8aa84e
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-23 13:04:22 +0100

    Update OSDCLOUD_StartURL.ps1

--- OSDCLOUD_StartURL.ps1
+++ OSDCLOUD_StartURL.ps1
@@ -1,62 +1,64 @@
-<#
-Naam: OSDCloud-Start.ps1
-Datum: 23-03-2025
-Beschrijving: Automatische installatie van Windows 11 via OSDCloud inclusief OOBEDeploy met RemoveAppx
-Novoferm Nederland BV
-#>
+#Requires -RunAsAdministrator
 
-# Forceer TLS1.2 voor internetverkeer
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# Zet execution policy tijdelijk op Bypass (alleen voor sessie)
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-
-# Startbericht
-Write-Host -ForegroundColor Cyan "Starten van OSDCloud..."
-Start-Sleep -Seconds 3
-
-# Modules installeren
-Install-Module OSD -Force -AllowClobber -Scope AllUsers
-Import-Module OSD -Force
-
-# Parameters voor Windows-installatie
-$OSDParams = @{
-    OSBuild     = "24H2"
-    OSEdition   = "Enterprise"
-    OSLanguage  = "nl-nl"
-    OSLicense   = "Volume"
-    ZTI         = $true
-    SkipAutopilot = $true
-    SkipODT       = $true
+$Automate = @'
+{
+    "BrandName":  "Novoferm Nederland",
+    "BrandColor":  "RED",
+    "OSActivation":  "Volume",
+    "OSEdition":  "Enterprise",
+    "OSLanguage":  "nl-nl",
+    "OSImageIndex":  6,
+    "OSName":  "Windows 11 24H2 x64",
+    "OSReleaseID":  "24H2",
+    "OSVersion":  "Windows 11",
+    "OSActivationValues":  [
+        "Retail",
+        "Volume"
+    ],
+    "OSEditionValues":  [
+        "Enterprise",
+        "Pro"
+    ],
+    "OSLanguageValues":  [
+        "nl-nl",
+        "en-us"
+    ],
+    "OSNameValues":  [
+        "Windows 11 24H2 x64",
+        "Windows 10 22H2 x64"
+    ],
+    "OSReleaseIDValues":  [
+        "24H2"
+    ],
+    "OSVersionValues":  [
+        "Windows 11",
+        "Windows 10"
+    ],
+    "ClearDiskConfirm":  false,
+    "restartComputer":  false,
+    "updateDiskDrivers":  false,
+    "updateFirmware":  false,
+    "updateNetworkDrivers":  true,
+    "updateSCSIDrivers":  true
 }
-Start-OSDCloud @OSDParams
+'@
 
-# Post-OS: Installeer OOBEDeploy en configureer AppX-verwijdering
-Install-Module OOBEDeploy -Force -AllowClobber -Scope AllUsers
-Import-Module OOBEDeploy -Force
 
-# OOBEDeploy uitvoeren met RemoveAppx lijst
-$OOBEParams = @{
-    Autopilot      = $false
-    RemoveAppx     = @(
-        "CommunicationsApps",     # Mail & Agenda
-        "OfficeHub",
-        "People",
-        "Skype",
-        "Solitaire",
-        "Xbox",
-        "ZuneMusic",
-        "ZuneVideo",
-        "OutlookForWindows",      
-        "YourPhone",
-        "MicrosoftWindows.Client.WebExperience"
-    )
-    UpdateDrivers  = $true
-    UpdateWindows  = $true
+$AutomateISO = "$(Get-OSDCloudWorkspace)\Media\OSDCloud\Automate"
+if (!(Test-Path $AutomateISO)) {
+    New-Item -Path $AutomateISO -ItemType Directory -Force
 }
-Start-OOBEDeploy @OOBEParams
+$Automate | Out-File -FilePath "$AutomateISO\Start-OSDCloudGUI.json" -Force
 
-# Reboot na afronden
-Write-Host -ForegroundColor Cyan "Windows wordt opnieuw opgestart in 30 seconden..."
-Start-Sleep -Seconds 30
-wpeutil reboot
+
+$AutomateUSB = "$(Get-OSDCloudWorkspace)\Media\Automate"
+if (!(Test-Path $AutomateUSB)) {
+    New-Item -Path $AutomateUSB -ItemType Directory -Force
+}
+$Automate | Out-File -FilePath "$AutomateUSB\Start-OSDCloudGUI.json" -Force
+
+# Run Edit-OSDCloudWinPE to rebuild
+Edit-OSDCloudWinPE -StartOSDCloudGUI
+
+# Test in a Virtual Machine
+New-OSDCloudVM
