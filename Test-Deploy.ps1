4d0ebba6f8a65f5e7028c5d5a0df7cdd580f872c
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-25 00:40:11 +0100

    Update Test-Deploy.ps1

--- Test-Deploy.ps1
+++ Test-Deploy.ps1
@@ -13,31 +13,41 @@ Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterp
 # Stap 2: Verwijder vooraf ongewenste AppX Provisioned Packages uit het geïnstalleerde image
 $TargetPath = "C:\"
 $apps = @(
-    'Clipchamp.Clipchamp',
-    'Microsoft.549981C3F5F10',
-    'Microsoft.BingNews',
-    'Microsoft.BingWeather',
-    'Microsoft.GamingApp',
-    'Microsoft.GetHelp',
-    'Microsoft.Getstarted',
-    'Microsoft.MicrosoftOfficeHub',
-    'Microsoft.MicrosoftSolitaireCollection',
-    'Microsoft.People',
-    'Microsoft.Todos',
-    'Microsoft.WindowsAlarms',
-    'microsoft.windowscommunicationsapps',
-    'Microsoft.WindowsFeedbackHub',
-    'Microsoft.WindowsMaps',
-    'Microsoft.Xbox.TCUI',
-    'Microsoft.XboxGameOverlay',
-    'Microsoft.XboxGamingOverlay',
-    'Microsoft.XboxIdentityProvider',
-    'Microsoft.XboxSpeechToTextOverlay',
-    'Microsoft.YourPhone',
-    'Microsoft.ZuneMusic',
-    'Microsoft.ZuneVideo',
-    'Microsoft.OutlookForWindows'  # Nieuwe Outlook
+    "Microsoft.549981C3F5F10"                # Cortana
+    "Microsoft.BingWeather"
+    "Microsoft.BingSearch"                   # BingSearch extensie
+    "Microsoft.GetHelp"
+    "Microsoft.Getstarted"                   # 'Aan de slag'
+    "Microsoft.Microsoft3DViewer"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.MixedReality.Portal"
+    "Microsoft.MSPaint"
+    "Microsoft.Office.OneNote"
+    "Microsoft.OneDrive"
+    "Microsoft.People"
+    "Microsoft.PowerAutomateDesktop"         # Power Automate Desktop
+    "Microsoft.SkypeApp"
+    "Microsoft.Todos"
+    "Microsoft.WindowsAlarms"
+    "Microsoft.WindowsCamera"
+    "microsoft.windowscommunicationsapps"    # Mail & Agenda
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsSoundRecorder"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.YourPhone"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "MicrosoftTeams"                         # Teams (Store app)
+    "Microsoft.OutlookForWindows"            # New Outlook
 )
+
 
 foreach ($app in $apps) {
     Get-AppxProvisionedPackage -Path $TargetPath | Where-Object DisplayName -eq $app | ForEach-Object {
