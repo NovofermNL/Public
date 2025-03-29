cf80a5c6103303d7402f07169e89ae1c58b2d91b
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-29 10:21:26 +0100

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -18,48 +18,46 @@ if ($env:SystemDrive -ne "X:") {
 Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force
 
-
-#    Istalleer Windows 11
+#   Installeer Windows 11
 Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
 
-#    Verwijder vooraf ongewenste AppX Provisioned Packages uit het geïnstalleerde image
+#   Verwijder vooraf ongewenste AppX Provisioned Packages uit het geïnstalleerde image
 $TargetPath = "C:\"
 $apps = @(
-    "Microsoft.549981C3F5F10"                # Cortana
-    "Microsoft.BingWeather"
-    "Microsoft.BingSearch"                   # BingSearch extensie
-    "Microsoft.GetHelp"
-    "Microsoft.Getstarted"                   # 'Aan de slag'
-    "Microsoft.Microsoft3DViewer"
-    "Microsoft.MicrosoftOfficeHub"
-    "Microsoft.MicrosoftSolitaireCollection"
-    "Microsoft.MicrosoftStickyNotes"
-    "Microsoft.MixedReality.Portal"
-    "Microsoft.MSPaint"
-    "Microsoft.Office.OneNote"
-    "Microsoft.OneDrive"
-    "Microsoft.People"
-    "Microsoft.PowerAutomateDesktop"         
-    "Microsoft.SkypeApp"
-    "Microsoft.Todos"
-    "Microsoft.WindowsAlarms"
-    "Microsoft.WindowsCamera"
-    "microsoft.windowscommunicationsapps"    
-    "Microsoft.WindowsFeedbackHub"
-    "Microsoft.WindowsMaps"
-    "Microsoft.WindowsSoundRecorder"
-    "Microsoft.Xbox.TCUI"
-    "Microsoft.XboxGameOverlay"
-    "Microsoft.XboxGamingOverlay"
-    "Microsoft.XboxIdentityProvider"
-    "Microsoft.XboxSpeechToTextOverlay"
-    "Microsoft.YourPhone"
-    "Microsoft.ZuneMusic"
-    "Microsoft.ZuneVideo"
-    "MicrosoftTeams"                         
-    "Microsoft.OutlookForWindows"            
+    "Microsoft.549981C3F5F10",               # Cortana
+    "Microsoft.BingWeather",
+    "Microsoft.BingSearch",                  # BingSearch extensie
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",                  # 'Aan de slag'
+    "Microsoft.Microsoft3DViewer",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.MicrosoftStickyNotes",
+    "Microsoft.MixedReality.Portal",
+    "Microsoft.MSPaint",
+    "Microsoft.Office.OneNote",
+    "Microsoft.OneDrive",
+    "Microsoft.People",
+    "Microsoft.PowerAutomateDesktop",        # Power Automate Desktop
+    "Microsoft.SkypeApp",
+    "Microsoft.Todos",
+    "Microsoft.WindowsAlarms",
+    "Microsoft.WindowsCamera",
+    "microsoft.windowscommunicationsapps",   # Mail & Agenda
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.WindowsSoundRecorder",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo",
+    "MicrosoftTeams",                        # Teams (Store app)
+    "Microsoft.OutlookForWindows"           # New Outlook
 )
-
 
 foreach ($app in $apps) {
     Get-AppxProvisionedPackage -Path $TargetPath | Where-Object DisplayName -eq $app | ForEach-Object {
