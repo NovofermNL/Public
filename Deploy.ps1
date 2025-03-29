5e0d4e4865c83e0f0d2c0dcef87e78c15c75168a
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-30 00:51:00 +0100

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,14 +1,6 @@
-<#
-Scriptnaam: Deploy.ps1
-Beschrijving: Installeert Windows 11 en verwijdert vooraf AppX provisioned packages
-Datum: 24-03-2025
-Organisatie: Novoferm Nederland BV
-#>
 
-#   PreOS - Set TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-#   Install and Import OSD Module (met WinPE-check)
 if ($env:SystemDrive -ne "X:") {
     Write-Host -ForegroundColor Green "Updating OSD PowerShell Module (buiten WinPE)"
     Install-Module OSD -Force 
@@ -19,8 +11,8 @@ if ($env:SystemDrive -ne "X:") {
 Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force
 
-#   Installeer Windows 11
+
 Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
 
-#   Herstart naar OOBE
+
 Restart-Computer
