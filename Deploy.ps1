805806fd8af75aa695f32c87e1daec25ec2ed35a
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-30 00:52:26 +0100

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,18 +1,3 @@
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-if ($env:SystemDrive -ne "X:") {
-    Write-Host -ForegroundColor Green "Updating OSD PowerShell Module (buiten WinPE)"
-    Install-Module OSD -Force 
-} else {
-    Write-Host -ForegroundColor Yellow "WinPE gedetecteerd – Install-Module OSD wordt overgeslagen"
-}
-
-Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force
-
-
 Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
 
-
-Restart-Computer
