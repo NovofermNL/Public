46f2e88706fd5efe6a1d95fd554a4b9b8f9242d5
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-03-30 00:46:58 +0100

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -22,49 +22,5 @@ Import-Module OSD -Force
 #   Installeer Windows 11
 Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
 
-#   Verwijder vooraf ongewenste AppX Provisioned Packages uit het geïnstalleerde image
-$TargetPath = "C:\"
-$apps = @(
-    "Microsoft.549981C3F5F10",
-    "Microsoft.BingWeather",
-    "Microsoft.BingSearch",
-    "Microsoft.GetHelp",
-    "Microsoft.Getstarted",
-    "Microsoft.Microsoft3DViewer",
-    "Microsoft.MicrosoftOfficeHub",
-    "Microsoft.MicrosoftSolitaireCollection",
-    "Microsoft.MicrosoftStickyNotes",
-    "Microsoft.MixedReality.Portal",
-    "Microsoft.MSPaint",
-    "Microsoft.Office.OneNote",
-    "Microsoft.OneDrive",
-    "Microsoft.People",
-    "Microsoft.PowerAutomateDesktop",
-    "Microsoft.SkypeApp",
-    "Microsoft.Todos",
-    "Microsoft.WindowsAlarms",
-    "Microsoft.WindowsCamera",
-    "microsoft.windowscommunicationsapps",
-    "Microsoft.WindowsFeedbackHub",
-    "Microsoft.WindowsMaps",
-    "Microsoft.WindowsSoundRecorder",
-    "Microsoft.Xbox.TCUI",
-    "Microsoft.XboxGameOverlay",
-    "Microsoft.XboxGamingOverlay",
-    "Microsoft.XboxIdentityProvider",
-    "Microsoft.XboxSpeechToTextOverlay",
-    "Microsoft.YourPhone",
-    "Microsoft.ZuneMusic",
-    "Microsoft.ZuneVideo",
-    "MicrosoftTeams",
-    "Microsoft.OutlookForWindows"
-)
-
-foreach ($app in $apps) {
-    Get-AppxProvisionedPackage -Path $TargetPath | Where-Object DisplayName -eq $app | ForEach-Object {
-        Remove-AppxProvisionedPackage -Path $TargetPath -PackageName $_.PackageName -ErrorAction SilentlyContinue
-    }
-}
-
 #   Herstart naar OOBE
 Restart-Computer
