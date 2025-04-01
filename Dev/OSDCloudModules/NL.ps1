6cd258bc73c37d6c3661f716d2a5808a8716f632
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-01 23:20:18 +0200

    Create NL.ps1

---
+++ Dev/OSDCloudModules/NL.ps1
@@ -0,0 +1,7 @@
+Write-Host -ForegroundColor Green "Installeren van de  OSDCloudLogic Module"
+New-Item -Path "X:\Program Files\WindowsPowerShell\Modules\OSDCloudLogic" -ItemType Directory -Force | Out-Null
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSDCloudModules/OSDCloud.psm1" -OutFile "X:\Program Files\WindowsPowerShell\Modules\OSDCloudLogic\OSDCloudLogic.psm1"
+Import-Module OSDCloudLogic.psm1 -Force
+
+Write-Host -ForegroundColor Green "Starting OSDCloud NL devices"
+OSDCloudLogic -ComputerPrefix "NL"
