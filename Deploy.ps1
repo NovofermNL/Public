e86b09892311c22719281d59d4f38e57b0151c6f
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-01 13:59:02 +0200

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -11,7 +11,7 @@ PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path=%PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-Start /Wait PowerShell -NoL -C Start-AutopilotOOBE
+:: Start /Wait PowerShell -NoL -C Start-AutopilotOOBE
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
