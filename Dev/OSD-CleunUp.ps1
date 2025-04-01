4894c3557bc6d0cbd2f54aa4765ee486dd0033d0
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-01 23:52:43 +0200

    Update OSD-CleunUp.ps1

--- Dev/OSD-CleunUp.ps1
+++ Dev/OSD-CleunUp.ps1
@@ -16,7 +16,7 @@ If (Test-Path -Path 'C:\OSDCloud\Logs') {
 If (Test-Path -Path 'C:\OSDCloud') { Remove-Item -Path 'C:\OSDCloud' -Recurse -Force }
 If (Test-Path -Path 'C:\Drivers') { Remove-Item 'C:\Drivers' -Recurse -Force }
 If (Test-Path -Path 'C:\Intel') { Remove-Item 'C:\Intel' -Recurse -Force }
-If (Test-Path -Path 'C:\ProgramData\OSDeploy') { Remove-Item 'C:\ProgramData\OSDeploy' -Recurse -Force }
+#If (Test-Path -Path 'C:\ProgramData\OSDeploy') { Remove-Item 'C:\ProgramData\OSDeploy' -Recurse -Force }
 
 
 Stop-Transcript
