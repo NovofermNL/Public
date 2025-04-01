3d3aaf2544367f67bc811713c03d35b262239634
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-01 08:57:01 +0200

    Update OSDCLOUD-Start.ps1

--- OSDCLOUD-Start.ps1
+++ OSDCLOUD-Start.ps1
@@ -1,7 +1,7 @@
 <#
 Naam: OSDCloud-Start.ps1
-Datum: 23-03-2025
-Beschrijving: Automatische installatie van Windows 11 via OSDCloud inclusief OOBEDeploy met RemoveAppx
+Datum: 01-04-2025
+Beschrijving: Automatische installatie van Windows 11 via OSDCloud inclusief OOBEDeploy met RemoveAppx via GitHub
 Novoferm Nederland BV
 #>
 
@@ -21,74 +21,20 @@ Import-Module OSD -Force
 
 # Parameters voor Windows-installatie
 $OSDParams = @{
-    OSBuild     = "24H2"
-    OSEdition   = "Enterprise"
-    OSLanguage  = "nl-nl"
-    OSLicense   = "Retail"
-    ZTI         = $true
+    OSBuild       = "24H2"
+    OSEdition     = "Enterprise"
+    OSLanguage    = "nl-nl"
+    OSLicense     = "Retail"
+    ZTI           = $true
     SkipAutopilot = $true
     SkipODT       = $true
 }
 Start-OSDCloud @OSDParams
 
-# Post-OS: Installeer OOBEDeploy en configureer AppX-verwijdering
-Install-Module OOBEDeploy -Force -AllowClobber -Scope AllUsers
+# OOBEDeploy installeren en extern script uitvoeren via GitHub
+Install-Module OOBEDeploy -Force 
 Import-Module OOBEDeploy -Force
-
-# OOBEDeploy uitvoeren met RemoveAppx lijst
-$OOBEParams = @{
-    Autopilot      = $false
-    RemoveAppx     = @(
-        "Clipchamp.Clipchamp",
-        "Microsoft.ApplicationCompatibilityEnhancements",
-        "Microsoft.AV1VideoExtension",
-        "Microsoft.AVCEncoderVideoExtension",
-        "Microsoft.BingNews",
-        "Microsoft.BingSearch",
-        "Microsoft.BingWeather",
-        "Microsoft.DesktopAppInstaller",
-        "Microsoft.GamingApp",
-        "Microsoft.GetHelp",
-        "Microsoft.HEIFImageExtension",
-        "Microsoft.HEVCVideoExtension",
-        "Microsoft.MicrosoftOfficeHub",                  # = OfficeHub
-        "Microsoft.MicrosoftSolitaireCollection",        # = Solitaire
-        "Microsoft.MicrosoftStickyNotes",
-        "Microsoft.MPEG2VideoExtension",
-        "Microsoft.OutlookForWindows",                   # = OutlookForWindows
-        "Microsoft.Paint",
-        "Microsoft.PowerAutomateDesktop",
-        "Microsoft.RawImageExtension",
-        "Microsoft.ScreenSketch",
-        "Microsoft.SecHealthUI",
-        "Microsoft.StorePurchaseApp",
-        "Microsoft.Todos",
-        "Microsoft.VP9VideoExtensions",
-        "Microsoft.WebMediaExtensions",
-        "Microsoft.WebpImageExtension",
-        "Microsoft.Windows.DevHome",
-        "Microsoft.Windows.Photos",
-        "Microsoft.WindowsAlarms",
-        "Microsoft.WindowsCalculator",
-        "Microsoft.WindowsCamera",
-        "Microsoft.WindowsFeedbackHub",
-        "Microsoft.WindowsNotepad",
-        "Microsoft.WindowsSoundRecorder",
-        "Microsoft.WindowsStore",
-        "Microsoft.WindowsTerminal",
-        "Microsoft.Xbox.TCUI",
-        "Microsoft.XboxGamingOverlay",
-        "Microsoft.XboxIdentityProvider",
-        "Microsoft.XboxSpeechToTextOverlay",
-        "Microsoft.YourPhone",
-        "Microsoft.ZuneMusic",
-        "MicrosoftCorporationII.QuickAssist",
-        "MicrosoftWindows.Client.WebExperience",
-        "MicrosoftWindows.CrossDevice",
-        "MSTeams"
-    )
-}
-Start-OOBEDeploy @OOBEParams
+Start-OOBEDeploy -PostAction 'irm https://raw.githubusercontent.com/NovofermNL/Public/main/OOBE/Remove-Appx.ps1 | iex'
 
 # Reboot na afronden
 Write-Host -ForegroundColor Cyan "Windows wordt opnieuw opgestart in 30 seconden..."
