819b5124102b917a41becea38df86b206bfb6286
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-01 23:51:52 +0200

    Update OSD-CleunUp.ps1

--- Dev/OSD-CleunUp.ps1
+++ Dev/OSD-CleunUp.ps1
@@ -1,16 +1,16 @@
 
 # Opruimen van OSD Deployment folders
 $Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Cleanup-Script.log"
-Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+Start-Transcript -Path (Join-Path "$env:ProgramData\OSDeploy\" $Global:Transcript) -ErrorAction Ignore
 
 Write-Host "Execute OSD Cloud Cleanup Script" -ForegroundColor Green
 
 # Kopieeren  OOBEDeploy en AutopilotOOBE Logs
-Get-ChildItem 'C:\Windows\Temp' -Filter *OOBE* | Copy-Item -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
+Get-ChildItem 'C:\Windows\Temp' -Filter *OOBE* | Copy-Item -Destination 'C:\ProgramData\OSDeploy' -Force
 
 # Kopieëren OSDCloud Logs
 If (Test-Path -Path 'C:\OSDCloud\Logs') {
-    Move-Item 'C:\OSDCloud\Logs\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
+    Move-Item 'C:\OSDCloud\Logs\*.*' -Destination 'C:\ProgramData\OSDeploy' -Force
 
 # Opruimen Folders
 If (Test-Path -Path 'C:\OSDCloud') { Remove-Item -Path 'C:\OSDCloud' -Recurse -Force }
