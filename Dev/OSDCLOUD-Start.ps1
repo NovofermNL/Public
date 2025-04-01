7a55462f2e1ceab6d2edd3f7bd408ed21074b253
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-01 21:22:13 +0200

    Update OSDCLOUD-Start.ps1

--- Dev/OSDCLOUD-Start.ps1
+++ Dev/OSDCLOUD-Start.ps1
@@ -129,9 +129,7 @@ Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Set-KeyboardLanguage.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Install-EmbeddedProductKey.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://check-autopilotprereq.osdcloud.ch
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://start-autopilotoobe.osdcloud.ch
+
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSD-CleunUp.ps1
