6bb9856ca7a5da0385c42a0b2fbebff5611e2457
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-01 23:52:10 +0200

    Update OSD-CleunUp.ps1

--- Dev/OSD-CleunUp.ps1
+++ Dev/OSD-CleunUp.ps1
@@ -6,11 +6,11 @@ Start-Transcript -Path (Join-Path "$env:ProgramData\OSDeploy\" $Global:Transcrip
 Write-Host "Execute OSD Cloud Cleanup Script" -ForegroundColor Green
 
 # Kopieeren  OOBEDeploy en AutopilotOOBE Logs
-Get-ChildItem 'C:\Windows\Temp' -Filter *OOBE* | Copy-Item -Destination 'C:\ProgramData\OSDeploy' -Force
+Get-ChildItem 'C:\Windows\Temp' -Filter *OOBE* | Copy-Item -Destination 'C:\ProgramData\OSDeploy\' -Force
 
 # Kopieëren OSDCloud Logs
 If (Test-Path -Path 'C:\OSDCloud\Logs') {
-    Move-Item 'C:\OSDCloud\Logs\*.*' -Destination 'C:\ProgramData\OSDeploy' -Force
+    Move-Item 'C:\OSDCloud\Logs\*.*' -Destination 'C:\ProgramData\OSDeploy\' -Force
 
 # Opruimen Folders
 If (Test-Path -Path 'C:\OSDCloud') { Remove-Item -Path 'C:\OSDCloud' -Recurse -Force }
