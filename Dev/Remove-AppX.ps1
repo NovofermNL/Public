a848465a774f347dadcd1f24974960e042019352
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-03 16:03:20 +0200

    Update Remove-AppX.ps1

--- Dev/Remove-AppX.ps1
+++ Dev/Remove-AppX.ps1
@@ -1,72 +1,12 @@
-<#
-Scriptnaam: Remove-AppxApps.ps1
-Datum: 23-03-2025
-Beschrijving: Verwijdert opgegeven AppX packages voor alle gebruikers tijdens OOBE.
-Auteur: Novoferm Nederland BV
-#>
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#Requires -Modules @{ ModuleName="OSD"; ModuleVersion="23.5.26.1" }
+#Requires -PSEdition Desktop
+#Requires -RunAsAdministrator
 
-$scriptName = "Remove-AppxApps"
-$logDir = "C:\windows\temp\$scriptName"
-if (!(Test-Path $logDir)) {
-    New-Item -ItemType Directory -Path $logDir -Force | Out-Null
-}
+#How To: Remove Appx Provisioned Packages by Match
 
-$logFile = "$logDir\log_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
-Start-Transcript -Path $logFile
 
-try {
-    $AppList = @(
-        "Microsoft.Xbox*",
-        "Microsoft.ZuneMusic",
-        "Microsoft.ZuneVideo",
-        "Microsoft.SkypeApp",
-        "Microsoft.BingNews",
-        "Microsoft.BingWeather",
-        "Microsoft.GetHelp",
-        "Microsoft.Getstarted",
-        "Microsoft.People",
-        "Microsoft.MicrosoftSolitaireCollection",
-        "Microsoft.Microsoft3DViewer",
-        "Microsoft.MixedReality.Portal",
-        "Microsoft.WindowsFeedbackHub",
-        "Microsoft.XboxGameOverlay",
-        "Microsoft.XboxGamingOverlay",
-        "Microsoft.XboxIdentityProvider",
-        "Microsoft.XboxSpeechToTextOverlay",
-        "Microsoft.YourPhone",
-        "Microsoft.MicrosoftOfficeHub",
-        "Microsoft.Todos",
-        "Microsoft.OneConnect",
-        "Microsoft.OutlookForWindows"
-    )
-
-    foreach ($App in $AppList) {
-        Write-Output "Verwerken: $App"
-
-        # Verwijder voor bestaande gebruikers
-        $existingPackages = Get-AppxPackage -AllUsers -Name $App
-        if ($existingPackages) {
-            foreach ($pkg in $existingPackages) {
-                Write-Output " - Verwijderen package: $($pkg.Name)"
-                Remove-AppxPackage -Package $pkg.PackageFullName -AllUsers -ErrorAction SilentlyContinue
-            }
-        }
-
-        # Verwijder provisioned package
-        $provisioned = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -like $App }
-        if ($provisioned) {
-            foreach ($prov in $provisioned) {
-                Write-Output " - Verwijderen provisioned package: $($prov.DisplayName)"
-                Remove-AppxProvisionedPackage -Online -PackageName $prov.PackageName -ErrorAction SilentlyContinue
-            }
-        }
-    }
-}
-catch {
-    Write-Output "Fout opgetreden tijdens verwerking: $_"
-}
-finally {
-    Stop-Transcript
+$RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
+foreach ($Item in $RemoveAppx) {
+    Remove-AppxOnline -Name $Item -Verbose
 }
