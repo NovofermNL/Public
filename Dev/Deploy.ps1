f2e2184609c8319b7ed408e277ba20bde265a5c0
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-03 16:40:49 +0200

    Update Deploy.ps1

--- Dev/Deploy.ps1
+++ Dev/Deploy.ps1
@@ -8,7 +8,7 @@
 # Installeer de OSD-module (alleen buiten WinPE)
 if ($env:SystemDrive -ne "X:") {
     Write-Host -ForegroundColor Green "Buiten WinPE gedetecteerd – OSD-module wordt geïnstalleerd"
-    Install-Module -Name OSD -Force -Scope CurrentUser
+    Install-Module -Name OSD -Force
 } else {
     Write-Host -ForegroundColor Yellow "WinPE gedetecteerd – Install-Module wordt overgeslagen"
 }
@@ -16,14 +16,52 @@ if ($env:SystemDrive -ne "X:") {
 # Importeer de OSD-module
 try {
     Write-Host -ForegroundColor Green "Importeren van OSD PowerShell Module..."
-    Import-Module -Name OSD -Force -ErrorAction Stop
+    Import-Module -Name OSD -Force 
     Write-Host -ForegroundColor Green "OSD-module succesvol geïmporteerd"
 }
 catch {
     Write-Host -ForegroundColor Red "Fout bij het importeren van de OSD-module: $_"
     exit 1
 }
+#  ---------------------------------------------------------------------------
+#  Profile OSD OSDDeploy
+#  ---------------------------------------------------------------------------
 
+if ($CustomProfile -in 'OSD','OSDDeploy') {
+    $AddNetFX3      = $true
+    $AddRSAT        = $False
+    $Autopilot      = $false
+    $UpdateDrivers  = $false
+    $UpdateWindows  = $false
+    $RemoveAppx     = @(
+        'Microsoft.549981C3F5F10',
+        'Microsoft.BingWeather',
+        'Microsoft.GetHelp',
+        'Microsoft.Getstarted',
+        'Microsoft.Microsoft3DViewer',
+        'Microsoft.MicrosoftOfficeHub',
+        'Microsoft.MicrosoftSolitaireCollection',
+        'Microsoft.MixedReality.Portal',
+        'Microsoft.Office.OneNote',
+        'Microsoft.People',
+        'Microsoft.SkypeApp',
+        'Microsoft.Wallet',
+        'Microsoft.WindowsCamera',
+        'microsoft.windowscommunicationsapps',
+        'Microsoft.WindowsFeedbackHub',
+        'Microsoft.WindowsMaps',
+        'Microsoft.Xbox.TCUI',
+        'Microsoft.XboxApp',
+        'Microsoft.XboxGameOverlay',
+        'Microsoft.XboxGamingOverlay',
+        'Microsoft.XboxIdentityProvider',
+        'Microsoft.XboxSpeechToTextOverlay',
+        'Microsoft.YourPhone',
+        'Microsoft.ZuneMusic',
+        'Microsoft.ZuneVideo'
+    )
+    $SetEdition     = 'Enterprise'
+}
 # Start installatie van Windows 11 via OSDCloud
 Write-Host -ForegroundColor Cyan "Start installatie van Windows 11..."
 Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
@@ -34,10 +72,10 @@ Write-Host -ForegroundColor Green "Maak C:\Windows\System32\OOBE.cmd aan"
 $OOBECMD = @'
 Start /Wait PowerShell -NoL -C PowerShell Set-ExecutionPolicy ByPass -Force
 ::Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+:: Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
 ::Start /Wait PowerShell -NoL -C Import-Module AutopilotOOBE -Force
-Start /Wait PowerShell -NoL -C Import-Module OSD -Force
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/Start-OOBE-Deploy.ps1
+::Start /Wait PowerShell -NoLogo -Command Import-Module OSD -Force
+Start /Wait PowerShell -NoLogo -Command Start-OOBEDeploy -Customprofile OSDDeploy
 '@
 
 $OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Force
