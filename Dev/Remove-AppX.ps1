8a9f136b33f976fd8aae943ac04a094b7328c2cc
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-03 20:58:28 +0200

    Update Remove-AppX.ps1

--- Dev/Remove-AppX.ps1
+++ Dev/Remove-AppX.ps1
@@ -1,12 +1,23 @@
+###################################################################
+########## Novoferm Nederland – Remove-AppX.ps1 ###################
+###################################################################
 
-#Requires -Modules @{ ModuleName="OSD"; ModuleVersion="23.5.26.1" }
-#Requires -PSEdition Desktop
-#Requires -RunAsAdministrator
+Write-Host "OOBE Phase – Appx-verwijdering gestart"
 
-#How To: Remove Appx Provisioned Packages by Match
+# Inlezen lijst vanuit JSON
+$apps = Get-Content 'C:\Windows\Temp\RemoveAppx.json' | ConvertFrom-Json
 
+foreach ($app in $apps) {
+    Write-Host "Verwijder Appx (per gebruiker): $app"
+    Get-AppxPackage -AllUsers -Name $app | Remove-AppxPackage -ErrorAction SilentlyContinue
 
-$RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo","Outlook"
-foreach ($Item in $RemoveAppx) {
-    Remove-AppxOnline -Name $Item -Verbose
+    Write-Host "Verwijder Appx (provisioned): $app"
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq $app | ForEach-Object {
+        Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName -ErrorAction SilentlyContinue
+    }
 }
+
+Write-Host "OOBE Phase – Appx-verwijdering voltooid"
+
+# OOBEDeploy uitvoeren voor eventuele extra setup
+Start-OOBEDeploy -CustomProfile OSDDeploy
