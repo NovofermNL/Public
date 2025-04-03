443c9476ca81855ac744db17db46ccf5e613532d
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-03 14:55:19 +0200

    Update Remove-AppX.ps1

--- Dev/Remove-AppX.ps1
+++ Dev/Remove-AppX.ps1
@@ -5,49 +5,68 @@ Beschrijving: Verwijdert opgegeven AppX packages voor alle gebruikers tijdens OO
 Auteur: Novoferm Nederland BV
 #>
 
-$logDir = "C:\temp\script-logging\Remove-AppxApps"
-New-Item -ItemType Directory -Path $logDir -Force | Out-Null
-Start-Transcript -Path "$logDir\log_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$AppList = @(
-    "Microsoft.Xbox*",
-    "Microsoft.ZuneMusic",
-    "Microsoft.ZuneVideo",
-    "Microsoft.SkypeApp",
-    "Microsoft.BingNews",
-    "Microsoft.BingWeather",
-    "Microsoft.GetHelp",
-    "Microsoft.Getstarted",
-    "Microsoft.People",
-    "Microsoft.MicrosoftSolitaireCollection",
-    "Microsoft.Microsoft3DViewer",
-    "Microsoft.MixedReality.Portal",
-    "Microsoft.WindowsFeedbackHub",
-    "Microsoft.XboxGameOverlay",
-    "Microsoft.XboxGamingOverlay",
-    "Microsoft.XboxIdentityProvider",
-    "Microsoft.XboxSpeechToTextOverlay",
-    "Microsoft.YourPhone",
-    "Microsoft.MicrosoftOfficeHub",
-    "Microsoft.Todos",
-    "Microsoft.OneConnect",
-    "Microsoft.OutlookForWindows"
-)
-
-foreach ($App in $AppList) {
-    Write-Host "`nVerwerken: $App" -ForegroundColor Yellow
-
-    # Verwijder voor bestaande gebruikers
-    Get-AppxPackage -AllUsers -Name $App | ForEach-Object {
-        Write-Host " - Verwijderen package: $($_.Name)"
-        Remove-AppxPackage -Package $_.PackageFullName -AllUsers -ErrorAction SilentlyContinue
-    }
-
-    # Verwijder provisioned package
-    Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like $App} | ForEach-Object {
-        Write-Host " - Verwijderen provisioned package: $($_.DisplayName)"
-        Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName -ErrorAction SilentlyContinue
-    }
+$scriptName = "Remove-AppxApps"
+$logDir = "C:\script-logging\$scriptName"
+if (!(Test-Path $logDir)) {
+    New-Item -ItemType Directory -Path $logDir -Force | Out-Null
 }
 
-Stop-Transcript
+$logFile = "$logDir\log_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
+Start-Transcript -Path $logFile
+
+try {
+    $AppList = @(
+        "Microsoft.Xbox*",
+        "Microsoft.ZuneMusic",
+        "Microsoft.ZuneVideo",
+        "Microsoft.SkypeApp",
+        "Microsoft.BingNews",
+        "Microsoft.BingWeather",
+        "Microsoft.GetHelp",
+        "Microsoft.Getstarted",
+        "Microsoft.People",
+        "Microsoft.MicrosoftSolitaireCollection",
+        "Microsoft.Microsoft3DViewer",
+        "Microsoft.MixedReality.Portal",
+        "Microsoft.WindowsFeedbackHub",
+        "Microsoft.XboxGameOverlay",
+        "Microsoft.XboxGamingOverlay",
+        "Microsoft.XboxIdentityProvider",
+        "Microsoft.XboxSpeechToTextOverlay",
+        "Microsoft.YourPhone",
+        "Microsoft.MicrosoftOfficeHub",
+        "Microsoft.Todos",
+        "Microsoft.OneConnect",
+        "Microsoft.OutlookForWindows"
+    )
+
+    foreach ($App in $AppList) {
+        Write-Output "Verwerken: $App"
+
+        # Verwijder voor bestaande gebruikers
+        $existingPackages = Get-AppxPackage -AllUsers -Name $App
+        if ($existingPackages) {
+            foreach ($pkg in $existingPackages) {
+                Write-Output " - Verwijderen package: $($pkg.Name)"
+                Remove-AppxPackage -Package $pkg.PackageFullName -AllUsers -ErrorAction SilentlyContinue
+            }
+        }
+
+        # Verwijder provisioned package
+        $provisioned = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -like $App }
+        if ($provisioned) {
+            foreach ($prov in $provisioned) {
+                Write-Output " - Verwijderen provisioned package: $($prov.DisplayName)"
+                Remove-AppxProvisionedPackage -Online -PackageName $prov.PackageName -ErrorAction SilentlyContinue
+            }
+        }
+    }
+}
+catch {
+    Write-Output "Fout opgetreden tijdens verwerking: $_"
+}
+finally {
+    Stop-Transcript
+}
