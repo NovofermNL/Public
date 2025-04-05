07a9a2a3241179c1327a1c872bba18aded66d9d2
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-05 13:54:51 +0200

    Update Remove-AppX.ps1

--- Dev/Remove-AppX.ps1
+++ Dev/Remove-AppX.ps1
@@ -1,23 +1,47 @@
-###################################################################
-########## Novoferm Nederland – Remove-AppX.ps1 ###################
-###################################################################
+  $RemoveAppx     = @(
+        'Microsoft.549981C3F5F10',
+        'Microsoft.BingWeather',
+        'Microsoft.GetHelp',
+        'Microsoft.Getstarted',
+        'Microsoft.Microsoft3DViewer',
+        'Microsoft.MicrosoftOfficeHub',
+        'Microsoft.MicrosoftSolitaireCollection',
+        'Microsoft.MixedReality.Portal',
+        'Microsoft.Office.OneNote',
+        'Microsoft.People',
+        'Microsoft.SkypeApp',
+        'Microsoft.Wallet',
+        'Microsoft.WindowsCamera',
+        'microsoft.windowscommunicationsapps',
+        'Microsoft.WindowsFeedbackHub',
+        'Microsoft.WindowsMaps',
+        'Microsoft.Xbox.TCUI',
+        'Microsoft.XboxApp',
+        'Microsoft.XboxGameOverlay',
+        'Microsoft.XboxGamingOverlay',
+        'Microsoft.XboxIdentityProvider',
+        'Microsoft.XboxSpeechToTextOverlay',
+        'Microsoft.YourPhone',
+        'Microsoft.ZuneMusic',
+        'Microsoft.ZuneVideo'
+    )
 
-Write-Host "OOBE Phase – Appx-verwijdering gestart"
+    # Schrijf lijst naar JSON-bestand dat later gebruikt wordt door OOBE.cmd
+    $RemoveAppx | ConvertTo-Json | Out-File -FilePath "C:\Windows\Temp\RemoveAppx.json" -Encoding ascii -Force
 
-# Inlezen lijst vanuit JSON
-$apps = Get-Content 'C:\Windows\Temp\RemoveAppx.json' | ConvertFrom-Json
+Start-Process powershell -ArgumentList "-NoLogo -Command Set-ExecutionPolicy Bypass -Force" -Wait
 
-foreach ($app in $apps) {
-    Write-Host "Verwijder Appx (per gebruiker): $app"
-    Get-AppxPackage -AllUsers -Name $app | Remove-AppxPackage -ErrorAction SilentlyContinue
 
-    Write-Host "Verwijder Appx (provisioned): $app"
-    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq $app | ForEach-Object {
+$RemoveAppx = Get-Content -Path 'C:\Windows\Temp\RemoveAppx.json' | ConvertFrom-Json
+
+foreach ($App in $RemoveAppx) {
+    Write-Host "Verwijder Appx voor alle gebruikers: $App"
+    Get-AppxPackage -AllUsers -Name $App | Remove-AppxPackage -ErrorAction SilentlyContinue
+
+    Write-Host "Verwijder Appx Provisioned Package: $App"
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq $App | ForEach-Object {
         Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName -ErrorAction SilentlyContinue
     }
 }
 
-Write-Host "OOBE Phase – Appx-verwijdering voltooid"
 
-# OOBEDeploy uitvoeren voor eventuele extra setup
-Start-OOBEDeploy -CustomProfile OSDDeploy
