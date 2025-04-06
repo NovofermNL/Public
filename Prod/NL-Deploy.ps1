e27b98219da83350d31980f2d6dd214802321d14
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-06 20:17:13 +0200

    Create NL-Deploy.ps1

---
+++ Prod/NL-Deploy.ps1
@@ -0,0 +1,51 @@
+###################################################################
+########## Novoferm Nederland W11-24h2 Deployment script ##########
+##########                   Dominic Bruins              ##########
+###################################################################
+
+###### Enable TLS 1.2 #######
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+###### Insall en import OSD module #######
+
+if ($env:SystemDrive -ne "X:") {
+    Write-Host -ForegroundColor Green "Buiten WinPE gedetecteerd – OSD-module wordt geinstalleerd"
+    Install-Module -Name OSD -Force
+} else {
+    Write-Host -ForegroundColor Yellow "WinPE gedetecteerd – Install-Module wordt overgeslagen"
+}
+
+try {
+    Write-Host -ForegroundColor Green "Importeren van OSD PowerShell Module..."
+    Import-Module -Name OSD -Force 
+    Write-Host -ForegroundColor Green "OSD-module succesvol geïmporteerd"
+}
+catch {
+    Write-Host -ForegroundColor Red "Fout bij het importeren van de OSD-module: $_"
+    exit 1
+}
+
+###### Start Windows 11 Installatioe ######
+
+Write-Host -ForegroundColor Cyan "Start installatie van Windows 11..."
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
+
+Write-Host -ForegroundColor Green "Downloading and creating script for OOBE phase"
+Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/Remove-AppX.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\Remove-AppX.ps1' -Encoding ascii -Force
+#invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSD-CleanUp.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\OSD-CleanUp.ps1' -Encoding ascii -Force
+
+###### Maken van OOBE.cmd #######
+
+$OOBECMD = @'
+@echo off
+:: OOBE fase – verwijder standaard apps
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Remove-AppX.ps1
+'@
+$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+
+####### Herstart na 20 seconden #######
+
+Write-Host -ForegroundColor Green "Herstart in 20 seconden..."
+Start-Sleep -Seconds 20
+wpeutil reboot
