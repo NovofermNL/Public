4092fffba4e638639c03ef66eb3991128b17eba9
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-16 12:14:46 +0200

    Update NL-Deploy.ps1

--- Prod/NL-Deploy.ps1
+++ Prod/NL-Deploy.ps1
@@ -33,7 +33,6 @@ Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/O
 Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Prod/OSDCleanUp.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\OSDCleanUp.ps1' -Encoding ascii -Force
 Copy-Item "X:\OSDCloud\Config\Run-Autopilot-Hash-Upload.cmd" -Destination "C:\Windows\System32\" -Force
 Copy-Item "X:\OSDCloud\Config\Autopilot-Hash-Upload.ps1" -Destination "C:\Windows\System32\" -Force
-#invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSD-CleanUp.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\OSD-CleanUp.ps1' -Encoding ascii -Force
 
 $OOBECMD = @'
 @echo off
