fe776d0a7be4c2b5ca8d4b447161a9a2aa8798e3
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-20 02:02:40 +0200

    Update OOBE.ps1

--- Dev/OOBE.ps1
+++ Dev/OOBE.ps1
@@ -1,8 +1,6 @@
-Write-Host  -ForegroundColor Cyan 'Windows 11 24H2 Pro Autopilot nl-nl'
-
-###################################################################
-########## Novoferm Nederland W11-24h2 Deployment script ##########
-###################################################################
+#=======================================================================
+#  Novoferm Nederland W11-24H2 Deployment Script (volledig)
+#=======================================================================
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -21,142 +19,108 @@ try {
 catch {
     Write-Host -ForegroundColor Red "Fout bij het importeren van de OSD-module: $_"
     exit 1
-}   
+}
 
 #=======================================================================
-#   [OS] Params and Start-OSDCloud
+#  Define extra variabelen (Product, Manufacturer etc.)
 #=======================================================================
-$Params = @{
-    OSVersion = "Windows 11"
-    OSBuild = "24H2"
-    OSEdition = "Enterprise"
-    OSLanguage = "nl-nl"
-    OSLicense = "Volume"
-    ZTI = $true
-}
-Start-OSDCloud @Params
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 
-#================================================
-#  [PostOS] Extra Scripts Downloaden (Copy-Start)
-#================================================
-Write-Host -ForegroundColor Green "Download Copy-Start.ps1 vanuit GitHub"
-Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSDCloudModules/Copy-Start.ps1 | Out-File -FilePath 'C:\Windows\Setup\Scripts\Copy-Start.ps1' -Encoding ascii -Force
-
-#================================================
-#  [PostOS] OOBEDeploy Configuration
-#================================================
-Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
-$OOBEDeployJson = @'
-{
-    "Autopilot": {
-        "IsPresent": false
-    },
-    "AddNetFX3": {
-        "IsPresent": true
-    },
-    "RemoveAppx": [
-        "Clipchamp.Clipchamp",
-        "Microsoft.BingNews",
-        "Microsoft.BingSearch",
-        "Microsoft.BingWeather",
-        "Microsoft.GamingApp",
-        "Microsoft.GetHelp",
-        "Microsoft.MicrosoftOfficeHub",
-        "Microsoft.MicrosoftSolitaireCollection",
-        "Microsoft.MicrosoftStickyNotes",
-        "Microsoft.OutlookForWindows",
-        "Microsoft.PowerAutomateDesktop",
-        "Microsoft.Todos",
-        "Microsoft.Windows.DevHome",
-        "Microsoft.WindowsAlarms",
-        "Microsoft.WindowsFeedbackHub",
-        "Microsoft.WindowsSoundRecorder",
-        "Microsoft.WindowsTerminal",
-        "Microsoft.Xbox.TCUI",
-        "Microsoft.XboxGamingOverlay",
-        "Microsoft.XboxIdentityProvider",
-        "Microsoft.XboxSpeechToTextOverlay",
-        "Microsoft.YourPhone",
-        "Microsoft.ZuneMusic"
-    ],
-    "UpdateDrivers": {
-        "IsPresent": true
-    },
-    "UpdateWindows": {
-        "IsPresent": true
-    }
+#=======================================================================
+#   OSDCLOUD VARS
+#=======================================================================
+$Global:MyOSDCloud = [ordered]@{
+    Restart               = [bool]$false
+    RecoveryPartition     = [bool]$true
+    OEMActivation         = [bool]$false 
+    WindowsUpdate         = [bool]$true
+    MSCatalogFirmware     = [bool]$true
+    WindowsUpdateDrivers  = [bool]$true
+    WindowsDefenderUpdate = [bool]$false
+    SetTimeZone           = [bool]$true
+    SkipClearDisk         = [bool]$false
+    ClearDiskConfirm      = [bool]$false
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB  = [bool]$true
+    CheckSHA1             = [bool]$true
 }
-'@
-If (!(Test-Path "C:\ProgramData\OSDeploy")) {
-    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
-}
-$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
 
-#================================================
-#  [PostOS] AutopilotOOBE Configuration Staging
-#================================================
-Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
-$AutopilotOOBEJson = @'
-{
-    "Assign": {
-        "IsPresent": true
-    },
-    "GroupTag": "Office",
-    "GroupTagOptions": [
-        "Office",
-        "Production"
-    ],
-    "Hidden": [
-        "AssignedComputerName",
-        "AssignedUser",
-        "PostAction",
-        "Assign",
-        "AddToGroup"
-    ],
-    "PostAction": "Quit",
-    "Run": "NetworkingWireless",
-    "Docs": "https://google.com/",
-    "Title": "Moelven Autopilot Registration"
+#=======================================================================
+#   Driver Pack Logica
+#=======================================================================
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion 'Windows 11' -OSReleaseID '24H2'
+if ($DriverPack) {
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
 }
-'@
-$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
 
-#================================================
-#  [PostOS] AutopilotOOBE CMD Command Line
-#================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\OOBE.cmd"
+$UseHPIA = $true
+if ($Manufacturer -match "HP" -and $UseHPIA) {
+    $Global:MyOSDCloud.HPTPMUpdate = $true
+    $Global:MyOSDCloud.HPIAALL = $true
+    $Global:MyOSDCloud.HPBIOSUpdate = $true
+    $Global:MyOSDCloud.HPCMSLDriverPackLatest = $true
+}
+
+if ($Manufacturer -match "HP") {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope AllUsers -Force 
+    Install-Module -Name PowerShellGet -Scope CurrentUser -AllowClobber -Force
+    Install-Module -Name HPCMSL -Force -Scope AllUsers -SkipPublisherCheck
+}
+
+Write-Output $Global:MyOSDCloud
+
+#=======================================================================
+#   Start OSDCloud met parameters
+#=======================================================================
+$OSVersion = 'Windows 11'
+$OSReleaseID = '24H2'
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'nl-nl'
+
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+#=======================================================================
+#   Scripts en Configuratie downloaden
+#=======================================================================
+Write-Host -ForegroundColor Green "Download OOBE scripts vanuit GitHub"
+Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/Remove-AppX.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\Remove-AppX.ps1' -Encoding ascii -Force
+Invoke-WebRequest -Uri "https://github.com/NovofermNL/Public/raw/main/Prod/start2.bin" -OutFile "C:\Windows\Setup\scripts\start2.bin"
+Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSDCloudModules/Copy-Start.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\Copy-Start.ps1' -Encoding ascii -Force
+Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Prod/OSDCleanUp.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\OSDCleanUp.ps1' -Encoding ascii -Force
+Copy-Item "X:\OSDCloud\Config\Run-Autopilot-Hash-Upload.cmd" -Destination "C:\Windows\System32\" -Force
+Copy-Item "X:\OSDCloud\Config\Autopilot-Hash-Upload.ps1" -Destination "C:\Windows\System32\" -Force
+
+#=======================================================================
+#   OOBE.cmd aanmaken
+#=======================================================================
 $OOBECMD = @'
-PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
-Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-Start /Wait PowerShell -NoL -C Import-Module OSD -Force
-Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-Start /Wait PowerShell -NoL -C Import-Module AutopilotOOBE -Force
-Start /Wait PowerShell -NoL -C C:\Windows\Setup\Scripts\Copy-Start.ps1
-Start /Wait PowerShell -NoL -C Start-OOBEDeploy
-Start /Wait PowerShell -NoL -C Start-AutopilotOOBE
-Start /Wait PowerShell -NoL -C Restart-Computer -Force
-'@
-If (!(Test-Path "C:\Windows\Setup\Scripts")) {
-    New-Item "C:\Windows\Setup\Scripts" -ItemType Directory -Force | Out-Null
-}
-$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\OOBE.cmd' -Encoding ascii -Force
-
-#================================================
-#  [PostOS] SetupComplete CMD Command Line
-#================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
-$SetupCompleteCMD = @'
 @echo off
-call C:\Windows\Setup\Scripts\OOBE.cmd
-RD C:\OSDCloud\OS /S /Q
-RD C:\Drivers /S /Q
+:: OOBE fase – verwijder standaard apps
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Remove-AppX.ps1
+:: OOBE fase – Aanpassen Start Menu
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Copy-Start.ps1
 '@
-$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
 
 #=======================================================================
-#   Restart-Computer
+#   SetupComplete.cmd aanmaken
 #=======================================================================
-Write-Host "Restarting in 20 seconds!" -ForegroundColor Green
+$SetupComplete = @'
+@echo off
+:: Laatste opruimtaken vóór eerste login
+powershell.exe -NoLogo -ExecutionPolicy Bypass -File "C:\Windows\Setup\scripts\OSDCleanUp.ps1"
+exit /b 0
+'@
+$SetupComplete | Out-File -FilePath 'C:\Windows\Setup\scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+#=======================================================================
+#   Herstart na 20 seconden
+#=======================================================================
+Write-Host -ForegroundColor Green "Herstart in 20 seconden..."
 Start-Sleep -Seconds 20
 wpeutil reboot
