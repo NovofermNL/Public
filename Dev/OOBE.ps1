fc89ceee2bbb74bc9ab1b484538f2e8ada824058
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-22 00:34:51 +0200

    Update OOBE.ps1

--- Dev/OOBE.ps1
+++ Dev/OOBE.ps1
@@ -1,13 +1,14 @@
-#=======================================================================
-#  Novoferm Nederland W11-24H2 Deployment Script (volledig)
-#=======================================================================
+###################################################################
+########## Novoferm Nederland W11-24h2 Deployment script ##########
+###################################################################
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 if ($env:SystemDrive -ne "X:") {
     Write-Host -ForegroundColor Green "Buiten WinPE gedetecteerd – OSD-module wordt geïnstalleerd"
     Install-Module -Name OSD -Force
-} else {
+}
+else {
     Write-Host -ForegroundColor Yellow "WinPE gedetecteerd – Install-Module wordt overgeslagen"
 }
 
@@ -21,35 +22,21 @@ catch {
     exit 1
 }
 
-#=======================================================================
-#  Define extra variabelen (Product, Manufacturer etc.)
-#=======================================================================
-$Product = (Get-MyComputerProduct)
-$Model = (Get-MyComputerModel)
-$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
-
 #Set OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$False
-    RecoveryPartition = [bool]$true
-    WindowsUpdate = [bool]$true
-    WindowsUpdateDrivers = [bool]$false
-    WindowsDefenderUpdate = [bool]$false
     ClearDiskConfirm = [bool]$False
-    }
+}
+ 
+#write variables to console
+$Global:MyOSDCloud
 
-#=======================================================================
-#   Start OSDCloud met parameters
-#=======================================================================
+#Variables bepalen welke windows versie wordt geinstalleerd. 
 $OSVersion = 'Windows 11'
 $OSReleaseID = '24H2'
 $OSName = 'Windows 11 24H2 x64'
 $OSEdition = 'Enterprise'
 $OSActivation = 'Volume'
 $OSLanguage = 'nl-nl'
-
-Write-Host "Starting OSDCloud" -ForegroundColor Green
-Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
 
 #================================================
 #  [PostOS] OOBEDeploy Configuration
@@ -61,36 +48,32 @@ $OOBEDeployJson = @'
                       "IsPresent":  true
                   },
     "Autopilot":  {
-                      "IsPresent":  true
+                      "IsPresent":  false
                   },
     "RemoveAppx":  [
-                    "MicrosoftTeams",
-                    "Microsoft.BingWeather",
-                    "Microsoft.BingNews",
-                    "Microsoft.GamingApp",
-                    "Microsoft.GetHelp",
-                    "Microsoft.Getstarted",
-                    "Microsoft.Messaging",
-                    "Microsoft.MicrosoftOfficeHub",
-                    "Microsoft.MicrosoftSolitaireCollection",
-                    "Microsoft.MicrosoftStickyNotes",
-                    "Microsoft.MSPaint",
-                    "Microsoft.People",
-                    "Microsoft.PowerAutomateDesktop",
-                    "Microsoft.StorePurchaseApp",
-                    "Microsoft.Todos",
-                    "microsoft.windowscommunicationsapps",
-                    "Microsoft.WindowsFeedbackHub",
-                    "Microsoft.WindowsMaps",
-                    "Microsoft.WindowsSoundRecorder",
-                    "Microsoft.Xbox.TCUI",
-                    "Microsoft.XboxGameOverlay",
-                    "Microsoft.XboxGamingOverlay",
-                    "Microsoft.XboxIdentityProvider",
-                    "Microsoft.XboxSpeechToTextOverlay",
-                    "Microsoft.YourPhone",
-                    "Microsoft.ZuneMusic",
-                    "Microsoft.ZuneVideo"
+    "Clipchamp.Clipchamp",
+    "Microsoft.BingNews",
+    "Microsoft.BingSearch",
+    "Microsoft.BingWeather",
+    "Microsoft.GamingApp",
+    "Microsoft.GetHelp",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.MicrosoftStickyNotes",
+    "Microsoft.OutlookForWindows",
+    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.Todos",
+    "Microsoft.Windows.DevHome",
+    "Microsoft.WindowsAlarms",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsSoundRecorder",
+    "Microsoft.WindowsTerminal",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic"
                    ],
     "UpdateDrivers":  {
                           "IsPresent":  true
@@ -105,96 +88,46 @@ If (!(Test-Path "C:\ProgramData\OSDeploy")) {
 }
 $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
 
-#=======================================================================
-#   Scripts en Configuratie downloaden
-#=======================================================================
-Write-Host -ForegroundColor Green "Download OOBE scripts vanuit GitHub"
-Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/Remove-AppX.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\Remove-AppX.ps1' -Encoding ascii -Force
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+#Write-Host -ForegroundColor Cyan "Start installatie van Windows 11..."
+#Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume -zti
+
+Write-Host -ForegroundColor Green "Downloading and creating script for OOBE phase"
+
 Invoke-WebRequest -Uri "https://github.com/NovofermNL/Public/raw/main/Prod/start2.bin" -OutFile "C:\Windows\Setup\scripts\start2.bin"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NovofermNL/Public/main/Update-HPDrivers.ps1" -OutFile "C:\Windows\Setup\scripts\Update-HPDrivers.ps1"
 Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSDCloudModules/Copy-Start.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\Copy-Start.ps1' -Encoding ascii -Force
 Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Prod/OSDCleanUp.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\OSDCleanUp.ps1' -Encoding ascii -Force
 Copy-Item "X:\OSDCloud\Config\Run-Autopilot-Hash-Upload.cmd" -Destination "C:\Windows\System32\" -Force
 Copy-Item "X:\OSDCloud\Config\Autopilot-Hash-Upload.ps1" -Destination "C:\Windows\System32\" -Force
-
-#=======================================================================
-#   OOBE Functionaliteiten
-#=======================================================================
-function Step-oobeUpdateDrivers {
-    [CmdletBinding()]
-    param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeUpdateDrivers -eq $true)) {
-        Write-Host -ForegroundColor Cyan 'Updating Windows Drivers'
-        if (!(Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore)) {
-            try {
-                Install-Module PSWindowsUpdate -Force
-                Import-Module PSWindowsUpdate -Force
-            } catch {
-                Write-Warning 'Unable to install PSWindowsUpdate Driver Updates'
-            }
-        }
-        if (Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore) {
-            Start-Process PowerShell.exe -ArgumentList "-Command Install-WindowsUpdate -UpdateType Driver -AcceptAll -IgnoreReboot" -Wait
-        }
-    }
-}
-function Step-oobeUpdateWindows {
-    [CmdletBinding()]
-    param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeUpdateWindows -eq $true)) {
-        Write-Host -ForegroundColor Cyan 'Updating Windows'
-        if (!(Get-Module PSWindowsUpdate -ListAvailable)) {
-            try {
-                Install-Module PSWindowsUpdate -Force
-                Import-Module PSWindowsUpdate -Force
-            } catch {
-                Write-Warning 'Unable to install PSWindowsUpdate Windows Updates'
-            }
-        }
-        if (Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore) {
-            Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
-            #Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -UpdateType Software -AcceptAll -IgnoreReboot'
-            #Install-WindowsUpdate -UpdateType Software -AcceptAll -IgnoreReboot -NotTitle 'Malicious'
-            #Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot'
-            #Start-Process PowerShell.exe -ArgumentList "-Command Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle 'Preview' -NotKBArticleID 'KB890830','KB5005463','KB4481252'" -Wait
-            Start-Process PowerShell.exe -ArgumentList "-Command Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle 'Preview'" -Wait
-
-        }
-    }
-}
+Copy-Item "X:\OSDCloud\Config\Manage-HP-Biossettings.ps1" -Destination "C:\Windows\Setup\scripts\Manage-HP-BIOS-Settings.ps1" -Force
 
 $OOBECMD = @'
 @echo off
-:: OOBE fase – verwijder standaard apps
-::start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Remove-AppX.ps1
-:: OOBE fase – Aanpassen Start Menu
+start /Wait PowerShell -NoLogo -ExecutionPolicy Bypass -File "C:\Windows\Setup\scripts\Manage-HP-BIOS-Settings.ps1" -SetSettings
 start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Copy-Start.ps1
+start /Wait PowerShell -NoLogo -ExecutionPolicy Bypass -File "C:\Windows\Setup\scripts\Update-HPDrivers.ps1"
 start /Wait PowerShell -NoLogo -Command Start-OOBEDeploy
 '@
 
 $OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
 
-#=======================================================================
-#   SetupComplete.cmd aanmaken
-#=======================================================================
+# SetupComplete – wordt uitgevoerd vóór eerste login
 $SetupComplete = @'
 @echo off
-:: Laatste opruimtaken vóór eerste login
+Start-Process PowerShell.exe -ArgumentList "-Command Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle 'Preview'" -Wait
 powershell.exe -NoLogo -ExecutionPolicy Bypass -File "C:\Windows\Setup\scripts\OSDCleanUp.ps1"
 exit /b 0
 '@
+
 $SetupComplete | Out-File -FilePath 'C:\Windows\Setup\scripts\SetupComplete.cmd' -Encoding ascii -Force
 
-#=======================================================================
-#   Herstart na 20 seconden
-#=======================================================================
+# Herstart na 20 seconden
 Write-Host -ForegroundColor Green "Herstart in 20 seconden..."
 Start-Sleep -Seconds 20
 wpeutil reboot
 
-
-#=======================================================================
-#   OOBE functies uitvoeren
-#=======================================================================
-Step-oobeUpdateDrivers
-Step-oobeUpdateWindows
 
