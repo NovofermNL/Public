2c53f5e2e02a6a94a4c41b70ea33dd74f5894292
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-22 10:35:15 +0200

    Update OSDCLOUD-Start.ps1

--- Dev/OSDCLOUD-Start.ps1
+++ Dev/OSDCLOUD-Start.ps1
@@ -1,140 +1,123 @@
 #================================================
-#   [PreOS] Update Module
+#   OSDCloud Build Sequence
+#   WARNING: Will wipe hard drive without prompt!!
+#   Windows 10 21H2 Pro en-gb Retail
+#   Deploys OS
+#   Updates OS
+#   Removes AppX Packages from OS
+#   No Office Deployment Tool
+#   Creates post deployment scripts for Autopilot
 #================================================
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-if ($env:SystemDrive -ne "X:") {
-    Write-Host -ForegroundColor Green "Buiten WinPE gedetecteerd – OSD-module wordt geïnstalleerd"
-    Install-Module -Name OSD -Force
-    Install-module -Name HPCMSL -force
-} else {
-    Write-Host -ForegroundColor Yellow "WinPE gedetecteerd – Install-Module wordt overgeslagen"
+#   PreOS
+#   Set VM Display Resolution
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
 }
-
-try {
-    Write-Host -ForegroundColor Green "Importeren van OSD PowerShell Module..."
-    Import-Module -Name OSD -Force
-    Import-Module -Name HPCMSL -force
-    Write-Host -ForegroundColor Green "OSD-module succesvol geïmporteerd"
-}
-catch {
-    Write-Host -ForegroundColor Red "Fout bij het importeren van de OSD-module: $_"
-    exit 1
-}
-
-#=======================================================================
-#   OSDCloud Global Setrtings
-#=======================================================================
-
-$Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$true
-    ClearDiskConfirm = [bool]$true
-    RecoveryPartition = [bool]$true
-    WindowsUpdate = [bool]$true
-    WindowsUpdateDrivers = [bool]$false
-    WindowsDefenderUpdate = [bool]$true
-    SetTimeZone = [bool]$true
-    ClearDiskConfirm = [bool]$false
-    ShutdownSetupComplete = [bool]$false
-    SyncMSUpCatDriverUSB = [bool]$false
-    CheckSHA1 = [bool]$true
-    HPIADrivers = [bool]$true
-}
-
-
-
-#=======================================================================
-#   [OS] Params and Start-OSDCloud
-#=======================================================================
+#================================================
+#   PreOS
+#   Install and Import OSD Module
+Install-Module OSD -Force
+Import-Module OSD -Force
+#================================================
+#   [OS] Start-OSDCloud with Params
+#================================================
 $Params = @{
-    OSVersion = "Windows 11"
-    OSBuild = "24H2"
-    OSEdition = "Enterprise"
-    OSLanguage = "nl-nl"
-    OSLicense = "Volume"
-    ZTI = $true
-    Firmware = $false
+    OSBuild = "21H2"
+    OSEdition = "Pro"
+    OSLanguage = "en-gb"
+    OSLicense = "Retail"
+    SkipAutopilot = $true
+    SkipODT = $true
+    ZTI = $True
 }
 Start-OSDCloud @Params
+#================================================
+#   WinPE PostOS Sample
+#   AutopilotOOBE Offline Staging
+#================================================
+Install-Module AutopilotOOBE -Force
+Import-Module AutopilotOOBE -Force
 
-#================================================
-#  [PostOS] OOBEDeploy Configuration
-#================================================
-Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
-$OOBEDeployJson = @'
-{
-    "AddNetFX3":  {
-                      "IsPresent":  true
-                  },
-    "Autopilot":  {
-                      "IsPresent":  true
-                  },
-    "RemoveAppx":  [
-                    "MicrosoftTeams",
-                    "Microsoft.BingWeather",
-                    "Microsoft.BingNews",
-                    "Microsoft.GamingApp",
-                    "Microsoft.GetHelp",
-                    "Microsoft.Getstarted",
-                    "Microsoft.Messaging",
-                    "Microsoft.MicrosoftOfficeHub",
-                    "Microsoft.MicrosoftSolitaireCollection",
-                    "Microsoft.MicrosoftStickyNotes",
-                    "Microsoft.MSPaint",
-                    "Microsoft.People",
-                    "Microsoft.PowerAutomateDesktop",
-                    "Microsoft.StorePurchaseApp",
-                    "Microsoft.Todos",
-                    "microsoft.windowscommunicationsapps",
-                    "Microsoft.WindowsFeedbackHub",
-                    "Microsoft.WindowsMaps",
-                    "Microsoft.WindowsSoundRecorder",
-                    "Microsoft.Xbox.TCUI",
-                    "Microsoft.XboxGameOverlay",
-                    "Microsoft.XboxGamingOverlay",
-                    "Microsoft.XboxIdentityProvider",
-                    "Microsoft.XboxSpeechToTextOverlay",
-                    "Microsoft.YourPhone",
-                    "Microsoft.ZuneMusic",
-                    "Microsoft.ZuneVideo"
-                   ],
-    "UpdateDrivers":  {
-                          "IsPresent":  true
-                      },
-    "UpdateWindows":  {
-                          "IsPresent":  true
-                      }
+$Params = @{
+    Title = 'Autopilot Registration'
+    GroupTagOptions = 'ISL'
+    Hidden = 'AddToGroup','AssignedComputerName','AssignedUser','PostAction'
+    Assign = $true
+    PostAction = 'Restart'
+    Run = 'PowerShell'
+    Disabled = 'Assign'
 }
-'@
-If (!(Test-Path "C:\ProgramData\OSDeploy")) {
-    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+AutopilotOOBE @Params
+#================================================
+#   WinPE PostOS Sample
+#   OOBEDeploy Offline Staging
+#================================================
+$Params = @{
+    Autopilot = $true
+    RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
+    UpdateDrivers = $true
+    UpdateWindows = $true
 }
-$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+Start-OOBEDeploy @Params
+#================================================
+#   WinPE PostOS
+#   Set OOBEDeploy CMD.ps1
+#================================================
+$SetCommand = @'
+@echo off
 
-#================================================
-#  [PostOS] AutopilotOOBE CMD Command Line
-#================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\Setup\scripts\oobe.cmd"
-$OOBECMD = @'
+:: Set the PowerShell Execution Policy
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-Start /Wait PowerShell -NoL -C Start-OOBEDeploy
-Start /Wait PowerShell -NoL -C Restart-Computer -Force
+
+:: Add PowerShell Scripts to the Path
+set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
+
+:: Open and Minimize a PowerShell instance just in case
+start PowerShell -NoL -W Mi
+
+:: Install the latest OSD Module
+start "Install-Module OSD" /wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+
+:: Start-OOBEDeploy
+:: The next line assumes that you have a configuration saved in C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json
+start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy
+
+exit
 '@
-$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+$SetCommand | Out-File -FilePath "C:\Windows\OOBEDeploy.cmd" -Encoding ascii -Force
+#================================================
+#   WinPE PostOS
+#   Set AutopilotOOBE CMD.ps1
+#================================================
+$SetCommand = @'
+@echo off
+
+:: Set the PowerShell Execution Policy
+PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+
+:: Add PowerShell Scripts to the Path
+set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
+
+:: Open and Minimize a PowerShell instance just in case
+start PowerShell -NoL -W Mi
+
+:: Install the latest AutopilotOOBE Module
+start "Install-Module AutopilotOOBE" /wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
+
+:: Start-AutopilotOOBE
+:: The next line assumes that you have a configuration saved in C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json
+start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE
+
+exit
+'@
+$SetCommand | Out-File -FilePath "C:\Windows\Autopilot.cmd" -Encoding ascii -Force
 
 #================================================
-#  [PostOS] SetupComplete CMD Command Line
+#   PostOS
+#   Shutdown-Computer & Display Message
 #================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
-$SetupCompleteCMD = @'
-powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
-powershell.exe -Command "& {IEX (IRM oobetasks.osdcloud.ch)}"
-'@
-$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
-
-#=======================================================================
-#   Restart-Computer
-#=======================================================================
-Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
-wpeutil reboot
+Write-Host -Foregroundcolor Red "IMPORTANT! - " -Nonewline
+Read-Host "Ensure to run the OOBEDeploy.cmd to complete the Autopilot readiness build, device will now shutdown
+,remember to update the boot order to HDD before proceeding with phase 2, press the ENTER key to continue...."
+Wpeutil Shutdown
