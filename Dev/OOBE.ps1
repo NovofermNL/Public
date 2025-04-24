de30eb07648c265fa66bcb684429fac535576018
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-25 00:56:58 +0200

    Update OOBE.ps1

--- Dev/OOBE.ps1
+++ Dev/OOBE.ps1
@@ -11,32 +11,7 @@ $null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -E
 $Global:oobeCloud = @{
     oobeAddCapability           = $true
     oobeAddCapabilityName       = 'NetFX'
-    oobeRemoveAppxPackage       = $true
-    oobeRemoveAppxPackageName   = @(
-        "Clipchamp.Clipchamp"
-        "Microsoft.BingNews"
-        "Microsoft.BingSearch"
-        "Microsoft.BingWeather"
-        "Microsoft.GamingApp"
-        "Microsoft.GetHelp"
-        "Microsoft.MicrosoftOfficeHub"
-        "Microsoft.MicrosoftSolitaireCollection"
-        "Microsoft.MicrosoftStickyNotes"
-        "Microsoft.OutlookForWindows"
-        "Microsoft.PowerAutomateDesktop"
-        "Microsoft.Todos"
-        "Microsoft.Windows.DevHome"
-        "Microsoft.WindowsAlarms"
-        "Microsoft.WindowsFeedbackHub"
-        "Microsoft.WindowsSoundRecorder"
-        "Microsoft.WindowsTerminal"
-        "Microsoft.Xbox.TCUI"
-        "Microsoft.XboxGamingOverlay"
-        "Microsoft.XboxIdentityProvider"
-        "Microsoft.XboxSpeechToTextOverlay"
-        "Microsoft.YourPhone"
-        "Microsoft.ZuneMusic"
-    )
+    oobeRemoveAppxPackage       = $false
     oobeUpdateDrivers           = $true
     oobeUpdateWindows           = $true
     oobeRestartComputer         = $true
