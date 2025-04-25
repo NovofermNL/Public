e10b7e5c4eadda4d2489a8665b8367002b839a0e
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-25 12:46:39 +0200

    Update w11.ps1

--- Dev/w11.ps1
+++ Dev/w11.ps1
@@ -75,8 +75,8 @@ $Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 $OSVersion = 'Windows 11' #Used to Determine Driver Pack
 $OSReleaseID = '24H2' #Used to Determine Driver Pack
 $OSName = 'Windows 11 24H2 x64'
-$OSEdition = 'Pro'
-$OSActivation = 'Enterprise'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
 $OSLanguage = 'nl-nl'
 
 
