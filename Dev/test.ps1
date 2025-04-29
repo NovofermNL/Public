e765bb70b5246da87432f9563d6da37074e10dfd
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-04-29 14:39:59 +0200

    Update test.ps1

--- Dev/test.ps1
+++ Dev/test.ps1
@@ -1,1 +1,205 @@
-test
+#to Run, boot OSDCloudUSB, at the PS Prompt: iex (irm win11.garytown.com)
+
+#region Initialization
+function Write-DarkGrayDate {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [System.String]
+        $Message
+    )
+    if ($Message) {
+        Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $Message"
+    }
+    else {
+        Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) " -NoNewline
+    }
+}
+function Write-DarkGrayHost {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [System.String]
+        $Message
+    )
+    Write-Host -ForegroundColor DarkGray $Message
+}
+function Write-DarkGrayLine {
+    [CmdletBinding()]
+    param ()
+    Write-Host -ForegroundColor DarkGray '========================================================================='
+}
+function Write-SectionHeader {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [System.String]
+        $Message
+    )
+    Write-DarkGrayLine
+    Write-DarkGrayDate
+    Write-Host -ForegroundColor Cyan $Message
+}
+function Write-SectionSuccess {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [System.String]
+        $Message = 'Success!'
+    )
+    Write-DarkGrayDate
+    Write-Host -ForegroundColor Green $Message
+}
+#endregion
+
+$ScriptName = 'Novoferm OSDCloud Deployment'
+$ScriptVersion = '25.01.22.1'
+Write-Host -ForegroundColor Green "$ScriptName $ScriptVersion"
+Invoke-Expression (Invoke-RestMethod functions.garytown.com) #Add custom functions used in Script Hosting in GitHub
+Invoke-Expression (Invoke-RestMethod functions.osdcloud.com) #Add custom fucntions from OSDCloud
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '24H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Pro'
+$OSActivation = 'Retail'
+$OSLanguage = 'en-us'
+
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+#Testing MS Update Catalog Driver Sync
+#$Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+
+#Used to Determine Driver Pack
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+
+if ($DriverPack){
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
+}
+#$Global:MyOSDCloud.DriverPackName = "None"
+
+<#If Drivers are expanded on the USB Drive, disable installing a Driver Pack
+if (Test-DISMFromOSDCloudUSB -eq $true){
+    Write-Host "Found Driver Pack Extracted on Cloud USB Flash Drive, disabling Driver Download via OSDCloud" -ForegroundColor Green
+    if ($Global:MyOSDCloud.SyncMSUpCatDriverUSB -eq $true){
+        write-host "Setting DriverPackName to 'Microsoft Update Catalog'"
+        $Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+    }
+    else {
+        write-host "Setting DriverPackName to 'None'"
+        $Global:MyOSDCloud.DriverPackName = "None"
+    }
+}
+#>
+#Enable HPIA | Update HP BIOS | Update HP TPM
+ 
+if (Test-HPIASupport){
+    Write-SectionHeader -Message "Detected HP Device, Enabling HPIA, HP BIOS and HP TPM Updates"
+    #$Global:MyOSDCloud.DevMode = [bool]$True
+    $Global:MyOSDCloud.HPTPMUpdate = [bool]$True
+    if ($Product -ne '83B2' -and $Model -notmatch "zbook"){$Global:MyOSDCloud.HPIAALL = [bool]$true} #I've had issues with this device and HPIA
+    #{$Global:MyOSDCloud.HPIAALL = [bool]$true}
+    $Global:MyOSDCloud.HPBIOSUpdate = [bool]$true
+    #$Global:MyOSDCloud.HPCMSLDriverPackLatest = [bool]$true #In Test 
+    #Set HP BIOS Settings to what I want:
+    Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Prod/Manage-HPBiosSettings.ps1)
+    Manage-HPBiosSettings -SetSettings -AcceptLicense
+}
+
+if ((Get-CimInstance -Class Win32_ComputerSystem).Manufacturer -like "*HP*") {
+    Write-Host "HP-systeem gedetecteerd, starten met driver update..."
+    Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/OSDeploy/OSD/master/Public/OSDCloudTS/Invoke-HPDriverUpdate.ps1')
+}
+else {
+    Write-Host "Geen HP-systeem, driver update wordt overgeslagen."
+}
+
+#write variables to console
+Write-SectionHeader "OSDCloud Variables"
+Write-Output $Global:MyOSDCloud
+
+#Launch OSDCloud
+Write-SectionHeader -Message "Starting OSDCloud"
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+Write-SectionHeader -Message "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot"
+
+Write-Host "Run post-install scrips"
+Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/NovofermNL/Public/refs/heads/main/Prod/Post-Install.ps1')
+
+
+$SetupComplete = @'
+@echo off
+:: Setup logging
+for /f %%a in ('powershell -NoProfile -Command "(Get-Date).ToString('yyyy-MM-dd-HHmmss')"') do set logname=%%a-Cleanup-Script.log
+set logfolder=C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD
+set logfile=%logfolder%\%logname%
+
+:: Zorg dat logmap bestaat
+if not exist "%logfolder%" mkdir "%logfolder%"
+
+echo === Start Cleanup %date% %time% === > "%logfile%"
+echo Execute OSD Cloud Cleanup Script >> "%logfile%"
+
+
+if exist "C:\Windows\Temp" (
+    copy /Y "C:\Windows\Temp\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+if exist "C:\Temp" (
+    copy /Y "C:\Temp\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+if exist "C:\OSDCloud\Logs" (
+    copy /Y "C:\OSDCloud\Logs\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+if exist "C:\ProgramData\OSDeploy" (
+    copy /Y "C:\ProgramData\OSDeploy\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+for %%D in (
+    "C:\OSDCloud"
+    "C:\Drivers"
+    "C:\Intel"
+    "C:\ProgramData\OSDeploy"
+) do (
+    if exist %%D (
+        echo Removing folder %%D >> "%logfile%"
+        rmdir /S /Q %%D >> "%logfile%" 2>&1
+    )
+)
+
+echo === Cleanup Voltooid %date% %time% === >> "%logfile%"
+
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Copy-Start.ps1
+
+
+exit /b 0
+'@
+
+$SetupComplete | Out-File -FilePath 'C:\Windows\Setup\scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+
+#Restart
+restart-computer
