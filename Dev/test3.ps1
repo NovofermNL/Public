53aba15835314444ff5d70e87ca0c296ccf16cd1
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-05-01 13:41:25 +0200

    Update test3.ps1

--- Dev/test3.ps1
+++ Dev/test3.ps1
@@ -166,6 +166,103 @@ Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation
 
 write-host "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot" -ForegroundColor Green
 
+##### TEST #####
+Write-Host -ForegroundColor Green "Downloading and creating script for OOBE phase"
+
+Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/Remove-AppX.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\Remove-AppX.ps1' -Encoding ascii -Force
+Invoke-WebRequest -Uri "https://github.com/NovofermNL/Public/raw/main/Prod/start2.bin" -OutFile "C:\Windows\Setup\scripts\start2.bin"
+Invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSDCloudModules/Copy-Start.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\Copy-Start.ps1' -Encoding ascii -Force
+#invoke-RestMethod https://raw.githubusercontent.com/NovofermNL/Public/main/Dev/OSD-CleanUp.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\OSD-CleanUp.ps1' -Encoding ascii -Force
+
+$OOBECMD = @'
+@echo off
+:: OOBE fase – verwijder standaard apps en wijzig start-menu
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Remove-AppX.ps1
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\scripts\Copy-Start.ps1
+'@
+$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+
+$SetupComplete = @'
+@echo off
+:: Setup logging
+for /f %%a in ('powershell -NoProfile -Command "(Get-Date).ToString('yyyy-MM-dd-HHmmss')"') do set logname=%%a-Cleanup-Script.log
+set logfolder=C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD
+set logfile=%logfolder%\%logname%
+
+:: Zorg dat logmap bestaat
+if not exist "%logfolder%" mkdir "%logfolder%"
+
+
+:: Zet drive naar C: voor zekerheid
+C:
+
+:: Blokkeer automatische installatie van extra apps
+echo Disabling Content Delivery Manager Features >> "%logfile%"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v ContentDeliveryAllowed /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v FeatureManagementEnabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v OemPreInstalledAppsEnabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v PreInstalledAppsEnabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v PreInstalledAppsEverEnabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SilentInstalledAppsEnabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338388Enabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338389Enabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338393Enabled /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+
+
+:: Cleanup logs en folders
+echo === Start Cleanup %date% %time% === >> "%logfile%"
+if exist "C:\Windows\Temp" (
+    copy /Y "C:\Windows\Temp\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+if exist "C:\Temp" (
+    copy /Y "C:\Temp\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+if exist "C:\OSDCloud\Logs" (
+    copy /Y "C:\OSDCloud\Logs\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+if exist "C:\ProgramData\OSDeploy" (
+    copy /Y "C:\ProgramData\OSDeploy\*.log" "%logfolder%" >> "%logfile%" 2>&1
+)
+
+for %%D in (
+    "C:\OSDCloud"
+    "C:\Drivers"
+    "C:\Intel"
+    "C:\ProgramData\OSDeploy"
+) do (
+    if exist %%D (
+        echo Removing folder %%D >> "%logfile%"
+        rmdir /S /Q %%D >> "%logfile%" 2>&1
+    )
+)
+
+:: Start copy-start script
+echo Starten van Copy-Start.ps1 >> "%logfile%"
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File "C:\Windows\Setup\scripts\Copy-Start.ps1" >> "%logfile%" 2>&1
+
+echo === SetupComplete Afgerond %date% %time% === >> "%logfile%"
+
+exit /b 0
+'@
+
+# Schrijf het SetupComplete script weg
+$SetupComplete | Out-File -FilePath 'C:\Windows\Setup\scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+
+# Herstart na 20 seconden
+Write-Host -ForegroundColor Green "Herstart in 20 seconden..."
+Start-Sleep -Seconds 20
+wpeutil reboot
+
+#   Herstart naar OOBE
+Restart-Computer
+
+###### EIDNE TEST ######
+
+
 #=======================================================================
 #   REBOOT DEVICE
 #=======================================================================
