cfcc207759a77e1f190d6d46110c5da52a729625
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-08-07 11:26:13 +0200

    Update NF-Update-HPDrivers.ps1

--- NovoTools/Functions/NF-Update-HPDrivers.ps1
+++ NovoTools/Functions/NF-Update-HPDrivers.ps1
@@ -1,15 +1,4 @@
-function NF-Update-HPDrivers {
-    <#
-    .SYNOPSIS
-        Update HP drivers op basis van het OSDCloudTS script van OSDeploy.
-    .DESCRIPTION
-        Voert automatisch de HP driver update uit als het systeem een HP is.
-        Gebruikt de publieke OSDCloudTS functie van OSDeploy GitHub.
-    .NOTES
-        Gebaseerd op: https://github.com/OSDeploy/OSD/blob/master/Public/OSDCloudTS/Invoke-HPDriverUpdate.ps1
-    .LINK
-        https://github.com/OSDeploy/OSD
-    #>
+function Update-HPDrivers{
 
     if ((Get-CimInstance -Class Win32_ComputerSystem).Manufacturer -like "*HP*") {
         Write-Host "HP-systeem gedetecteerd, starten met driver update..."
