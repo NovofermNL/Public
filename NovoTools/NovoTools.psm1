cecdd7921be203c5b10152f0ef63b5c40ff45ec7
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-08-09 00:18:14 +0200

    Update NovoTools.psm1

--- NovoTools/NovoTools.psm1
+++ NovoTools/NovoTools.psm1
@@ -1,10 +1,11 @@
-# Lijst van functies (zonder .ps1 extensie!)
-$functionList = @(
-    "Install-NFWindowsUpdates"
-)
-
 foreach ($function in $functionList) {
     $url = "https://raw.githubusercontent.com/NovofermNL/Public/main/NovoTools/Functions/$function.ps1"
+
+    if (-not (Invoke-WebRequest -Uri $url -UseBasicParsing -Method Head -ErrorAction SilentlyContinue)) {
+        Write-Warning "Functiebestand niet gevonden: $url"
+        continue
+    }
+
     try {
         Invoke-Expression (Invoke-RestMethod -Uri $url -UseBasicParsing)
     }
