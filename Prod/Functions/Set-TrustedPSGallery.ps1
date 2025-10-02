395274b1e233d61a167f82e3b0e9ec25fa217826
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-10-02 23:48:55 +0200

    Update and rename TrustPSGallery.ps1 to Set-TrustedPSGallery.ps1

---
+++ Prod/Functions/Set-TrustedPSGallery.ps1
@@ -0,0 +1,22 @@
+function Set-TrustedPSGallery {
+    <#
+    Scriptnaam: Set-TrustedPSGallery
+    Beschrijving: Controleert of de PSGallery repository op Trusted staat. 
+                  Zo niet, dan wordt de NuGet provider geïnstalleerd en de repository op Trusted gezet.
+    #>
+
+    [CmdletBinding()]
+    param()
+
+    # Zorg dat TLS 1.2 wordt gebruikt (best practice)
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    if (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+        Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+        Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+        Write-Output "PSGallery repository is nu ingesteld als Trusted."
+    }
+    else {
+        Write-Output "PSGallery repository is al Trusted."
+    }
+}
--- Prod/Functions/TrustPSGallery.ps1
+++
@@ -1,4 +0,0 @@
-if (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
-    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
-    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
-}
