a1bb5dae767b87984102622a5756614e07e16de2
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-10-02 23:47:55 +0200

    Create TrustPSGallery.ps1

---
+++ Prod/Functions/TrustPSGallery.ps1
@@ -0,0 +1,4 @@
+if (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+}
